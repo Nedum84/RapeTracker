b9627403740af4acbe0e297ee41d24f1fd884929
Nedum84 <nelsonnedum@gmail.com>
2020-07-09 02:24:39 +0100

    Log complain form 2 finished

--- app/src/main/java/com/ng/rapetracker/ui/fragment/act_main/FragmentLogComplainForm2TypeOfVictim.kt
+++ app/src/main/java/com/ng/rapetracker/ui/fragment/act_main/FragmentLogComplainForm2TypeOfVictim.kt
@@ -5,24 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.happinesstonic.viewmodel.ModelHomeActivity
-import com.ng.rapetracker.R
 import com.ng.rapetracker.adapter.*
 import com.ng.rapetracker.databinding.FragmentLogComplainForm2TypeOfVictimBinding
-import com.ng.rapetracker.databinding.FragmentMainBinding
 import com.ng.rapetracker.model.RapeDetail
-import com.ng.rapetracker.model.RapeType
 import com.ng.rapetracker.ui.fragment.act_main.FragmentLogComplainForm2TypeOfVictimArgs.fromBundle
-import com.ng.rapetracker.utils.ClassAlertDialog
-import com.ng.rapetracker.viewmodel.GetRapeDetailViewModel
 import com.ng.rapetracker.viewmodel.RapeComplainFormViewModel
-import com.ng.rapetracker.viewmodel.factory.GetRapeDetailViewModelFactory
 
 
 class FragmentLogComplainForm2TypeOfVictim : Fragment() {
@@ -65,70 +56,13 @@ class FragmentLogComplainForm2TypeOfVictim : Fragment() {
         binding.rapeDetail = rapeDetail
 
         ADAPTER = AdapterTypeOfVictim(RapeTypeOfVictimClickListener {
-            rapeDetail.typeOfVictim = it.typeOfVictim
+            rapeDetail.typeOfVictim = it._id
             this.findNavController().navigate(FragmentLogComplainForm2TypeOfVictimDirections.actionFragmentLogComplainForm2TypeOfVictimToFragmentLogComplainForm3TypeOfRape(rapeDetail))
         })
-        binding.recyclerRapeTypeOfVictim.adapter = ADAPTER
-
-        return binding.root
-    }
-
-
-}
-
-
-class FragmentMain : Fragment() {
-    val linearLayoutManager by lazy {
-        LinearLayoutManager(activity)
-    }
-
-    lateinit var ADAPTER : AdapterRapeDetail
-    lateinit var binding: FragmentMainBinding
-    lateinit var getRapeViewModel: GetRapeDetailViewModel
-
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
-
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
-
-        val application = requireNotNull(this.activity).application
-        val viewModelFactory = GetRapeDetailViewModelFactory(application)
-        getRapeViewModel = ViewModelProvider(this, viewModelFactory).get(GetRapeDetailViewModel::class.java)
-
-        binding.lifecycleOwner = this
-
-
-        ADAPTER = AdapterRapeDetail(RapeDetailClickListener {
-            ClassAlertDialog(application).toast("Rape Detail Clicked...!!!")
-        })
-        binding.rapeDetailList.apply {
+        binding.recyclerRapeTypeOfVictim.apply {
             adapter = ADAPTER
-            layoutManager=linearLayoutManager
+            layoutManager=LinearLayoutManager(activity)
         }
-
-        getRapeViewModel.allRapeDetails.observe(viewLifecycleOwner, Observer {
-            it?.let {
-                if (it.isEmpty()){
-                    binding.noComplainWrapper.visibility = View.VISIBLE
-                }else{
-                    ADAPTER.addNewItems(it)
-                }
-                ClassAlertDialog(application).toast("refresh from Main Frag ... 1st observer...!!!")
-            }
-        })
-
-
-        getRapeViewModel.allRapeDetails2.observe(viewLifecycleOwner, Observer {
-            ClassAlertDialog(application).toast("refresh from Main Frag ... second observer...!!!")
-        })
-
-
-        binding.logComplainBtn.setOnClickListener {
-            this.findNavController().navigate(FragmentMainDirections.actionFragmentMainToFragmentLogComplainForm1RapeVictim())
-        }
-
-
-
 
         return binding.root
     }
--- app/src/main/java/com/ng/rapetracker/ui/fragment/act_main/FragmentLogComplainForm3TypeOfRape.kt
+++ app/src/main/java/com/ng/rapetracker/ui/fragment/act_main/FragmentLogComplainForm3TypeOfRape.kt
@@ -5,56 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ng.rapetracker.R
+import com.ng.rapetracker.adapter.AdapterRapeType
+import com.ng.rapetracker.adapter.AdapterTypeOfVictim
+import com.ng.rapetracker.adapter.RapeTypeClickListener
+import com.ng.rapetracker.adapter.RapeTypeOfVictimClickListener
+import com.ng.rapetracker.databinding.FragmentLogComplainForm2TypeOfVictimBinding
+import com.ng.rapetracker.databinding.FragmentLogComplainForm3TypeOfRapeBinding
+import com.ng.rapetracker.model.RapeDetail
+import com.ng.rapetracker.viewmodel.RapeComplainFormViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentLogComplainForm3TypeOfRape.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentLogComplainForm3TypeOfRape : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var rapeComplainFormViewModel: RapeComplainFormViewModel
+    lateinit var ADAPTER : AdapterRapeType
+    lateinit var binding: FragmentLogComplainForm3TypeOfRapeBinding
+    lateinit var rapeDetail: RapeDetail
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_complain_form3_type_of_rape, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentLogComplainForm3TypeOfRape.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentLogComplainForm3TypeOfRape().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        rapeComplainFormViewModel.allRapeType.observe(viewLifecycleOwner, Observer {
+            it?.apply {
+                ADAPTER?.rapeTypes = it
             }
+        })
+
     }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        binding = FragmentLogComplainForm3TypeOfRapeBinding.inflate(inflater)
+
+        val application = requireNotNull(activity).application
+        binding.lifecycleOwner = this
+        rapeDetail = arguments.let { FragmentLogComplainForm3TypeOfRapeArgs.fromBundle(it!!).updatedRapeDetail}
+        val viewModelFactory = RapeComplainFormViewModel.Factory(rapeDetail, application)
+        rapeComplainFormViewModel = ViewModelProvider(this, viewModelFactory).get(
+            RapeComplainFormViewModel::class.java)
+
+
+
+        ADAPTER = AdapterRapeType(RapeTypeClickListener {
+            rapeDetail.typeOfRape = it._id
+            this.findNavController().navigate(FragmentLogComplainForm3TypeOfRapeDirections.actionFragmentLogComplainForm3TypeOfRapeToFragmentLogComplainForm4SelectSupport(rapeDetail))
+        })
+        binding.recyclerRapeType.apply {
+            adapter = ADAPTER
+            layoutManager= LinearLayoutManager(activity)
+        }
+
+        return binding.root
+    }
+
+
 }
